c0662e5677020686875ce418ff6cb0b159f62707
Dhruva Sagar <dhruva.sagar@gmail.com>
2019-11-07 17:05:11 -0500

    Allow hoisted route mappings
    When we request for a url and it includes more content after a slash in
    the format `/{key}/{rest:.*}`, then we assume it was a service hoisted
    on this key so we redirect to the mapped url and append the rest to it's
    url to forward the request to the service with the rest of the url as
    requested. This obviously is only good for GET requests.

--- routes/redirect.go
+++ routes/redirect.go
@@ -27,6 +27,28 @@ func redirect(st *store.Store) http.HandlerFunc {
 	}
 }
 
+func redirectService(st *store.Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		key := vars["key"]
+		rest := vars["rest"]
+		urlMap, err := st.GetURLMap(key)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusNotImplemented)
+			return
+		}
+		if urlMap == nil {
+			err := fmt.Errorf("No URL mapped to key: %q", key)
+			http.Error(w, err.Error(), http.StatusNotImplemented)
+			return
+		}
+		url := fmt.Sprintf("%s/%s", urlMap.URL, rest)
+		fmt.Println("URL : ", url)
+		http.Redirect(w, r, url, http.StatusSeeOther)
+	}
+}
+
 func InitRedirect(r *mux.Router, st *store.Store) {
 	r.HandleFunc("/{key}", redirect(st)).Methods("GET")
+	r.HandleFunc("/{key}/{rest:.*}", redirectService(st)).Methods("GET")
 }
--- routes/redirect_test.go
+++ routes/redirect_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func setupRedirect(dst *store.Store) (*httptest.ResponseRecorder, *store.Store) {
+func setupRedirect(dst *store.Store) (*mux.Router, *httptest.ResponseRecorder) {
 	w := httptest.NewRecorder()
 	r := mux.NewRouter()
 
@@ -24,15 +24,25 @@ func setupRedirect(dst *store.Store) (*httptest.ResponseRecorder, *store.Store) 
 
 	InitRedirect(r, st)
 
+	return r, w
+}
+
+func setupRedirectKey(st *store.Store) (*httptest.ResponseRecorder, *store.Store) {
+	r, w := setupRedirect(st)
 	req := httptest.NewRequest("GET", "/test", nil)
-
 	r.ServeHTTP(w, req)
+	return w, st
+}
 
+func setupRedirectService(st *store.Store) (*httptest.ResponseRecorder, *store.Store) {
+	r, w := setupRedirect(st)
+	req := httptest.NewRequest("GET", "/test/service/route", nil)
+	r.ServeHTTP(w, req)
 	return w, st
 }
 
 func TestRedirectFailure(t *testing.T) {
-	w, st := setupRedirect(nil)
+	w, st := setupRedirectKey(nil)
 	defer st.Close()
 	defer test.CleanTestStore()
 
@@ -62,7 +72,7 @@ func TestRedirectSuccessful(t *testing.T) {
 
 	setupURLMap(st)
 
-	w, _ := setupRedirect(st)
+	w, _ := setupRedirectKey(st)
 
 	teardownURLMap(st)
 
@@ -70,6 +80,32 @@ func TestRedirectSuccessful(t *testing.T) {
 		t.Error("Did not get expected HTTP status code, got", w.Code)
 	}
 	if w.Body.String() != "<a href=\"/\">See Other</a>.\n\n" {
+		t.Error("Did not get expected HTTP response body, got", w.Body.String())
+	}
+}
+
+func setupURLServiceMap(st *store.Store) {
+	st.SaveURLMap(store.URLMap{
+		Key: "test",
+		URL: "http://localhost:8080",
+	})
+}
+
+func TestRedirectService(t *testing.T) {
+	st := test.NewTestStore()
+	defer st.Close()
+	defer test.CleanTestStore()
+
+	setupURLServiceMap(st)
+
+	w, _ := setupRedirectService(st)
+
+	teardownURLMap(st)
+
+	if w.Code != http.StatusSeeOther {
+		t.Error("Did not get expected HTTP status code, got", w.Code)
+	}
+	if w.Body.String() != "<a href=\"http://localhost:8080/service/route\">See Other</a>.\n\n" {
 		t.Error("Did not get expected HTTP response body, got", w.Body.String())
 	}
 }
